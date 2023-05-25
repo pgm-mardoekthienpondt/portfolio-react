3668235678b9a93a10ba67ab6341c4dd9617bab4
Mardoek Thienpondt <mardoek.thienpondt@student.arteveldehs.be>
2023-05-25 13:31:47 +0200

    first commit

--- src/app/components/main/main.jsx
+++ src/app/components/main/main.jsx
@@ -1,44 +1,64 @@
 import React from 'react';
-import "./main.scss";
+import './main.scss';
 const Main = () => {
-    return (
-        <main>
-            <section>
-                <h2>I am most familiar with</h2>
-                <ul>
-                    <li>
-                        <img src="/img/css-3-svgrepo-com.svg" alt="css logo" />
-                    </li>
-                    <li>
-                        <img src="/img/html-5-svgrepo-com.svg" alt="html logo" />
-                    </li>
-                    <li>
-                        <img src="/img/javascript-svgrepo-com.svg" alt="javascript logo" />
-                    </li>
-                    <li>
-                        <img src="/img/react-javascript-js-framework-facebook-svgrepo-com.svg" alt="react logo" />
-                    </li>
-                </ul>
-            </section>
-            <section>
-                <h2>Projects I have worked on</h2>
-                <ul>
-                    <li className='card' style={{width: 18 + 'rem'}}>
-                        <img src="/img/schrauwen-groen.PNG" className="card-img-top" alt="Schauwen Groen logo"/>
-                        <div className="card-body">
-                            <p className="card-text">Website @work1</p>
-                        </div>
-                    </li>
-                    <li className='card' style={{width: 18 + 'rem'}}>
-                        <img src="/img/gentse-feesten.PNG" className="card-img-top" alt="Gentse Feesten logo"/>
-                        <div className="card-body">
-                            <p className="card-text">Website @work1</p>
-                        </div>
-                    </li>
-                </ul>
-            </section>
-        </main>
-    );
+  return (
+    <main>
+      <section>
+        <h2>I am most familiar with</h2>
+        <ul>
+          <li>
+            <img
+              src="/portfolio-react/img/css-3-svgrepo-com.svg"
+              alt="css logo"
+            />
+          </li>
+          <li>
+            <img
+              src="/portfolio-react/img/html-5-svgrepo-com.svg"
+              alt="html logo"
+            />
+          </li>
+          <li>
+            <img
+              src="/portfolio-react/img/javascript-svgrepo-com.svg"
+              alt="javascript logo"
+            />
+          </li>
+          <li>
+            <img
+              src="/portfolio-react/img/react-javascript-js-framework-facebook-svgrepo-com.svg"
+              alt="react logo"
+            />
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h2>Projects I have worked on</h2>
+        <ul>
+          <li className="card" style={{ width: 18 + 'rem' }}>
+            <img
+              src="/portfolio-react/img/schrauwen-groen.PNG"
+              className="card-img-top"
+              alt="Schauwen Groen logo"
+            />
+            <div className="card-body">
+              <p className="card-text">Website @work1</p>
+            </div>
+          </li>
+          <li className="card" style={{ width: 18 + 'rem' }}>
+            <img
+              src="/portfolio-react/img/gentse-feesten.PNG"
+              className="card-img-top"
+              alt="Gentse Feesten logo"
+            />
+            <div className="card-body">
+              <p className="card-text">Website @work1</p>
+            </div>
+          </li>
+        </ul>
+      </section>
+    </main>
+  );
 };
 
 export default Main;
